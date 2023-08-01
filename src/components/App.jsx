2cf796c9ba705d4fb682f1596011fdc2f14f70dd
olhazamlynska <olya.sonechko@gmail.com>
2023-08-01 19:05:25 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,16 @@
-const App = () => {
-  return <div>Event class</div>;
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
 };
-
-export default App;
